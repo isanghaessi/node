3a303b7acf2cc8a3119caf9e0b9582ab06516a90
hong seungyong <jesse1231@naver.com>
2021-07-27 01:08:24 +0900

    remove db info

--- work0/gulpfile.js
+++ work0/gulpfile.js
@@ -1,18 +1,18 @@
-const { resolveInclude } = require('ejs');
+const {resolveInclude} = require('ejs');
 const gulp = require('gulp');
 
 gulp.task('default', async () => {
-	const Sequelize_auto = require('sequelize-auto');
-	const sequelize_auto = new Sequelize_auto('test', 'test', 'test', {
-		host: '3.36.243.240',
-		port: '3306',
-		dialect: 'mysql'
-	});
-	sequelize_auto.run((err) => {
-		if (err) {
-			throw err;
-		}
-	});
+    const Sequelize_auto = require('sequelize-auto');
+    const sequelize_auto = new Sequelize_auto('test', 'test', 'test', {
+        host: 'host',
+        port: '3306',
+        dialect: 'mysql',
+    });
+    sequelize_auto.run((err) => {
+        if (err) {
+            throw err;
+        }
+    });
 });
 // -----> sequelize-cli 커맨드
 // sequelize-auto -o "./models" -d chois -h localhost -u root -p 3306 -x tmddyd -e mysql
--- work1/gulpfile.js
+++ work1/gulpfile.js
@@ -1,15 +1,15 @@
 const gulp = require('gulp');
 
 gulp.task('default', async () => {
-	const Sequelize_auto = require('sequelize-auto');
-	const sequelize_auto = new Sequelize_auto('test', 'test', 'test', {
-		host: '3.36.243.240',
-		port: '3306',
-		dialect: 'mysql'
-	});
-	sequelize_auto.run((err) => {
-		if (err) {
-			throw err;
-		}
-	});
+    const Sequelize_auto = require('sequelize-auto');
+    const sequelize_auto = new Sequelize_auto('test', 'test', 'test', {
+        host: 'host',
+        port: '3306',
+        dialect: 'mysql',
+    });
+    sequelize_auto.run((err) => {
+        if (err) {
+            throw err;
+        }
+    });
 });
